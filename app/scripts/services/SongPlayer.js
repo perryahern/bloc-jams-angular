e0ade472aabc1ae34546f40c8fe4f07bee01d0d4
perryahern <pahern@gmail.com>
2017-05-08 19:07:58 -0400

    Assignment Angular 7 Services Part 2: completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -2,7 +2,12 @@
     function SongPlayer() {
         var SongPlayer = {};
 
+        /**
+        * @desc Object holding current song information
+        * @type {Object}
+        */
         var currentSong = null;
+
         /**
         * @desc Buzz object audio file
         * @type {Object}
@@ -28,14 +33,25 @@
             currentSong = song;
         };
 
+        /**
+        * @function playSong
+        * @desc Plays the currently paused song
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            currentBuzzObject.play();
+            song.playing = true;
+        };
+
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
                     currentBuzzObject.play();
+                } else {
+                    currentBuzzObject.pause();
                 }
             }
          };
